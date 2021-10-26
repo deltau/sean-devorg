c14562b57a0333700f98f2498d38130660b75e60
smcinturff <sean.mcinturff@gmail.com>
2021-10-26 13:51:34 -0600

    Added Collateral.  Product controls Collateral.

--- force-app/main/default/lwc/lwCollateralId/lwCollateralId.js
+++ force-app/main/default/lwc/lwCollateralId/lwCollateralId.js
@@ -5,28 +5,35 @@
 import {api, LightningElement, track, wire} from 'lwc';
 import getSolutionDomains from '@salesforce/apex/lwCollateralIdController.getSolutionDomains';
 import getProducts from '@salesforce/apex/lwCollateralIdController.getProducts';
+import getCollateral from '@salesforce/apex/lwCollateralIdController.getCollateral';
 
 export default class LwCollateralId extends LightningElement {
     @api location;
-    @track currentSolutionDomainId;
+    @track selectedSolutionDomainId;
+    @track selectedProductId;
     @track mySolutionDomains=[];
     @track currentProducts=[];
+    @track currentCollateral=[];
+    @track collateralDisabled = true;
+
     productSet = [];
+    collateralSet = [];
     error;
 
     defaultSolutionDomain = 'inProgress';
     defaultProduct = '';
+    @track defaultCollateral = '';
 
     @wire(getSolutionDomains)
     wireSolutionDomains({data,error}) {
         if(data) {
-            console.log('wireRecord');
+            console.log('getSolutionDomains');
             console.log(data);
 
             let mySolutionDomains=[];
 
             for (let key of data) {
-                console.log(key);
+                // console.log(key);
                 mySolutionDomains.push({label: key.Name, value: key.Id});
 
             }
@@ -55,7 +62,7 @@ export default class LwCollateralId extends LightningElement {
             console.log(data);
 
             for (let key of data) {
-                console.log(key);
+                // console.log(key);
                 this.productSet.push(key);
                 myProducts.push({label: key.Name, value: key.Id});
             }
@@ -68,10 +75,37 @@ export default class LwCollateralId extends LightningElement {
         }
     }
 
+    @wire(getCollateral)
+    wireCollateral({data, error}) {
+
+        if(data) {
+            let myCollateral = [];
+
+            console.log('getCollateral');
+            console.log(data);
+
+            for (let key of data) {
+                console.log(key);
+                this.collateralSet.push(key);
+                myCollateral.push({label: key.UCM_Collateral__r.Name, value: key.UCM_Collateral__c});
+            }
+
+            this.currentCollateral = myCollateral;
+            this.error = undefined;
+        } else {
+            this.error = error;
+            this.data = undefined;
+        }
+    }
+
     get products() {
         return this.currentProducts;
     }
 
+    get collateral() {
+        return this.currentCollateral;
+    }
+    
     get solutionDomains() {
         // let mySolutionDomains=[];
 
@@ -96,37 +130,80 @@ export default class LwCollateralId extends LightningElement {
         console.log(event.target);
         console.log('Name: ' + event.target.name);
         console.log('Target Value: ' + event.target.value);
-        console.log('--- DETAIL ---');
-        console.log(event.detail);
         console.log('Detail Value: ' + event.detail.value);
 
+        switch (event.target.name) {
+            case 'SOLUTION-DOMAIN':
+                this.selectedSolutionDomainId = event.target.value;
+                this.selectedProductId = '';
+                this.defaultCollateral = '';
+                this.collateralDisabled = true;
+                console.log('1');
 
-        if (event.target.name === 'SOLUTION-DOMAIN') {
+                console.log('4');
+                this.currentProducts = this.reevaluateProducts();
+                this.currentCollateral = this.reevaluateCollateral();
+                console.log('5');
+                break;
 
-            this.currentSolutionDomainId = event.target.value;
-            console.log('1');
-
-            console.log('4');
-            this.currentProducts = this.reevaluateProducts();
-            console.log('5');
+            case 'PRODUCT':
+                this.selectedProductId = event.target.value;
+                this.defaultCollateral = '';
+                this.currentCollateral = this.reevaluateCollateral();
+                break;
         }
+        // if (event.target.name === 'SOLUTION-DOMAIN') {
+        //
+        //     this.selectedSolutionDomainId = event.target.value;
+        //     console.log('1');
+        //
+        //     console.log('4');
+        //     this.currentProducts = this.reevaluateProducts();
+        //     console.log('5');
+        // }
+
         this.value = event.detail.value;
     }
 
     reevaluateProducts() {
+        console.log('reevaluateProducts');
         let myProducts = [];
 
         for (let product of this.productSet) {
             console.log('2');
             // console.log(product);
-            if (product.UCM_Solution_Domain__c === this.currentSolutionDomainId) {
+            if (product.UCM_Solution_Domain__c === this.selectedSolutionDomainId) {
                 console.log('3');
                 myProducts.push({label: product.Name, value: product.Id});
             }
         }
 
+        // if (myProducts.length > 0) {
+        //     this.collateralDisabled = false;
+        // }
         return myProducts;
     }
 
+    reevaluateCollateral() {
+        console.log('reevaluateCollateral');
+        console.log('selectedProductId: ' + this.selectedProductId);
+        console.log(this.collateralSet);
+
+        let myCollateral = [];
+
+        for (let collateral of this.collateralSet) {
+            console.log('2');
+            // console.log(product);
+            if (collateral.UCM_Products__c === this.selectedProductId) {
+                console.log('3');
+                myCollateral.push({label: collateral.UCM_Collateral__r.Name, value: collateral.UCM_Collateral__c});
+            }
+        }
+        console.log(myCollateral);
+        if (myCollateral.length > 0) {
+            this.collateralDisabled = false;
+        }
+        return myCollateral;
+    }
 
 }
